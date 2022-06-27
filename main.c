006c9cdc3956d6c582e666591d0a81618b099c8a
Doc011997 <50246766+Doc011997@users.noreply.github.com>
2022-06-28 01:13:55 +0200

    Add files via upload

---
+++ main.c
@@ -0,0 +1,24 @@
+//
+//  main.c
+//  Mastercamp
+//
+//  Created by Nathanaël Benjamin on 18/05/2022.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int nbr1;
+int nbr2;
+
+
+int main () {
+   ;
+    
+ 
+           
+}
+    
+    
+   
